a5781d27cbc7245df0d7a2eccecf8ba804b7ee30
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-08-24 19:48:40 +0300

    imp - Updated licensing

--- BassBoom.Basolia/InitBasolia.cs
+++ BassBoom.Basolia/InitBasolia.cs
@@ -1,4 +1,22 @@
-﻿using BassBoom.Native.Interop.Init;
+﻿
+//   BassBoom  Copyright (C) 2023  Aptivi
+// 
+//   This file is part of BassBoom
+// 
+//   BassBoom is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+// 
+//   BassBoom is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+// 
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using BassBoom.Native.Interop.Init;
 using BassBoom.Native.Runtime;
 
 namespace BassBoom.Basolia
