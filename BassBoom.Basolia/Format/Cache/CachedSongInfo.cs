d7383691008600cdd0db76af6f49eca6ad931e9e
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-12-10 12:50:32 +0300

    upd - Updated copyright year

--- BassBoom.Basolia/Format/Cache/CachedSongInfo.cs
+++ BassBoom.Basolia/Format/Cache/CachedSongInfo.cs
@@ -1,20 +1,21 @@
-﻿
-//   BassBoom  Copyright (C) 2023  Aptivi
-// 
-//   This file is part of BassBoom
-// 
-//   BassBoom is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-// 
-//   BassBoom is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-// 
-//   You should have received a copy of the GNU General Public License
-//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+﻿//
+// BassBoom  Copyright (C) 2023  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// BassBoom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BassBoom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 using BassBoom.Basolia.Lyrics;
 
