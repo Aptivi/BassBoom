4588629ab48c491b3354568a6533e36f5a68b43d
Aptivi <eoflaoevicecity@gmail.com>
2024-03-02 14:00:32 +0300

    add - Added scaffolding code for synthesis CLI
    ---
    
    We've added scaffolding code for the synthesis CLI using BassBoom Basolia.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ BassBoom.Synthesis/Program.cs
@@ -0,0 +1,29 @@
+﻿//
+// BassBoom  Copyright (C) 2023  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// BassBoom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BassBoom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Colors.Data;
+using Terminaux.Writer.ConsoleWriters;
+
+internal class Program
+{
+    private static void Main()
+    {
+        TextWriterColor.WriteColor("Synthesis is coming soon. Please wait until Alpha 7 comes...", ConsoleColors.Red);
+    }
+}
