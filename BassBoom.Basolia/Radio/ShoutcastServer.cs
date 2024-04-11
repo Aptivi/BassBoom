7343de9ea15248782671a8eb2df47de8d497b97e
Aptivi <eoflaoevicecity@gmail.com>
2024-04-11 22:29:51 +0300

    add - doc - Added SHOUTcast radio tools from Nettify
    ---
    
    We've added SHOUTcast radio tools from Nettify. This allows us to add internet radio support for BassBoom.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Part: 1/1

---
+++ BassBoom.Basolia/Radio/ShoutcastServer.cs
@@ -0,0 +1,246 @@
+﻿//
+// BassBoom  Copyright (C) 2023  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// BassBoom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BassBoom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace BassBoom.Basolia.Radio
+{
+    /// <summary>
+    /// A Shoutcast server
+    /// </summary>
+    [DebuggerDisplay("{ServerHostFull,nq}: S[T:{TotalStreams}|A:{ActiveStreams}] | L[{CurrentListeners}]")]
+    public class ShoutcastServer
+    {
+        private ShoutcastVersion serverVersion;
+        private int totalStreams;
+        private int activeStreams;
+        private int currentListeners;
+        private int peakListeners;
+        private int maxListeners;
+        private int uniqueListeners;
+        private int averageTime;
+        private readonly List<StreamInfo> streams = [];
+        internal JToken streamToken;
+        internal HtmlDocument streamHtmlToken = new();
+        internal HttpClient client = new();
+
+        /// <summary>
+        /// Server IP address
+        /// </summary>
+        public string ServerHost { get; }
+        /// <summary>
+        /// Server port
+        /// </summary>
+        public int ServerPort { get; }
+        /// <summary>
+        /// Whether the Shoutcast server is using HTTPS or not
+        /// </summary>
+        public bool ServerHttps { get; }
+        /// <summary>
+        /// Server IP address with port
+        /// </summary>
+        public string ServerHostFull =>
+            ServerHost + ":" + ServerPort;
+        /// <summary>
+        /// Server version (1.x, 2.x)
+        /// </summary>
+        public ShoutcastVersion ServerVersion =>
+            serverVersion;
+        /// <summary>
+        /// Total number of streams in the server
+        /// </summary>
+        public int TotalStreams =>
+            totalStreams;
+        /// <summary>
+        /// Active streams in the server
+        /// </summary>
+        public int ActiveStreams =>
+            activeStreams;
+        /// <summary>
+        /// How many people are listening to the server at this time?
+        /// </summary>
+        public int CurrentListeners =>
+            currentListeners;
+        /// <summary>
+        /// How many listeners did the server ever get at peak times?
+        /// </summary>
+        public int PeakListeners =>
+            peakListeners;
+        /// <summary>
+        /// How many people can listen to the server?
+        /// </summary>
+        public int MaxListeners =>
+            maxListeners;
+        /// <summary>
+        /// How many unique listeners are there?
+        /// </summary>
+        public int UniqueListeners =>
+            uniqueListeners;
+        /// <summary>
+        /// Average time on any active listener connections in seconds
+        /// </summary>
+        public int AverageTime =>
+            averageTime;
+        /// <summary>
+        /// Average time on any active listener connections in the time span
+        /// </summary>
+        public TimeSpan AverageTimeSpan =>
+            TimeSpan.FromSeconds(AverageTime);
+        /// <summary>
+        /// Available streams and their statistics
+        /// </summary>
+        public StreamInfo[] Streams =>
+            [.. streams];
+
+        /// <summary>
+        /// Connects to the Shoutcast server and gets the information
+        /// </summary>
+        /// <param name="serverHost">Server host name</param>
+        /// <param name="serverPort">Server port</param>
+        /// <param name="useHttps">Whether to use the HTTPS protocol or not</param>
+        public ShoutcastServer(string serverHost, int serverPort, bool useHttps)
+        {
+            // Check to see if we're dealing with the secure Shoutcast server
+            ServerHost = serverHost;
+            if (useHttps)
+            {
+                if (!serverHost.Contains(Uri.SchemeDelimiter))
+                    ServerHost = $"https://{serverHost}";
+            }
+            else
+            {
+                if (!serverHost.Contains(Uri.SchemeDelimiter))
+                    ServerHost = $"http://{serverHost}";
+            }
+            ServerHttps = useHttps;
+
+            // Install the values initially
+            ServerPort = serverPort;
+            serverVersion = ShoutcastVersion.v2;
+        }
+
+        /// <summary>
+        /// Refreshes the statistics
+        /// </summary>
+        public void Refresh()
+        {
+            try
+            {
+                InitializeStatsAsync().Wait();
+
+                // Determine version of Shoutcast
+                if (serverVersion == ShoutcastVersion.v1)
+                    FinalizeShoutcastV1();
+                else
+                    FinalizeShoutcastV2();
+            }
+            catch (Exception ex)
+            {
+                throw new ShoutcastServerException($"Failed to parse Shoutcast server {ServerHost}. More information can be found in the inner exception.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Refreshes the statistics
+        /// </summary>
+        public async Task RefreshAsync()
+        {
+            try
+            {
+                await InitializeStatsAsync();
+
+                // Determine version of Shoutcast
+                if (serverVersion == ShoutcastVersion.v1)
+                    FinalizeShoutcastV1();
+                else
+                    FinalizeShoutcastV2();
+            }
+            catch (Exception ex)
+            {
+                throw new ShoutcastServerException($"Failed to parse Shoutcast server {ServerHost}. More information can be found in the inner exception.", ex);
+            }
+        }
+
+        internal async Task InitializeStatsAsync()
+        {
+            // Use the full address to download the statistics. Note that Shoutcast v2 streams will use the /statistics directory, which provides
+            // more information than /7.html. If we're dealing with the first version, or if /statistics is disabled for some reason, fallback to
+            // /7.html
+            Uri statisticsUri = new(ServerHostFull + "/statistics?json=1");
+            Uri fallbackUri = new(ServerHostFull + "/7.html");
+            string serverResponse = await client.GetStringAsync(statisticsUri);
+
+            // Shoutcast v1.x doesn't have /statistics...
+            if (serverResponse.Contains("Invalid resource"))
+            {
+                // Detected v1. Fallback to /7.html
+                serverVersion = ShoutcastVersion.v1;
+                serverResponse = await client.GetStringAsync(fallbackUri);
+                streamHtmlToken.LoadHtml(serverResponse);
+            }
+            else
+                streamToken = JToken.Parse(serverResponse);
+        }
+
+        internal void FinalizeShoutcastV1()
+        {
+            // Shoutcast version v1.x, so use the html fallback token. Response values are as follows:
+            // currentlisteners,streamstatus(S),peaklisteners,maxlisteners,uniquelisteners,bitrate(S),songtitle(S)
+            // First, deal with the server settings.
+            string[] response = streamHtmlToken.DocumentNode.SelectSingleNode("body").InnerText.Split(',');
+            currentListeners = Convert.ToInt32(response[0]);
+            peakListeners = Convert.ToInt32(response[2]);
+            maxListeners = Convert.ToInt32(response[3]);
+            uniqueListeners = Convert.ToInt32(response[4]);
+
+            // Then, deal with the stream settings
+            StreamInfo streamInfo = new(this, null);
+            streams.Clear();
+            streams.Add(streamInfo);
+        }
+
+        internal void FinalizeShoutcastV2()
+        {
+            // Shoutcast version v2.x, so use the JToken.
+            // Use all the keys in the first object except the "streams" and "version", where we'd later use the former in StreamInfo to install
+            // all the streams into the new class instance.
+            totalStreams = (int)streamToken["totalstreams"];
+            activeStreams = (int)streamToken["activestreams"];
+            currentListeners = (int)streamToken["currentlisteners"];
+            peakListeners = (int)streamToken["peaklisteners"];
+            maxListeners = (int)streamToken["maxlisteners"];
+            uniqueListeners = (int)streamToken["uniquelisteners"];
+            averageTime = (int)streamToken["averagetime"];
+
+            // Now, deal with the stream settings.
+            streams.Clear();
+            foreach (JToken stream in streamToken["streams"])
+            {
+                StreamInfo streamInfo = new(this, stream);
+                streams.Add(streamInfo);
+            }
+        }
+    }
+}
---
+++ BassBoom.Basolia/Radio/StreamInfo.cs
@@ -0,0 +1,213 @@
+﻿//
+// BassBoom  Copyright (C) 2023  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// BassBoom is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// BassBoom is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+
+namespace BassBoom.Basolia.Radio
+{
+    /// <summary>
+    /// Shoutcast stream information
+    /// </summary>
+    [DebuggerDisplay("{StreamTitle,nq} is now playing {SongTitle} | L[{CurrentListeners}]")]
+    public class StreamInfo
+    {
+        private readonly int currentListeners;
+        private readonly int peakListeners;
+        private readonly int maxListeners;
+        private readonly int uniqueListeners;
+        private readonly int averageTime;
+        private readonly int streamId;
+        private readonly string streamGenre;
+        private readonly string streamGenre2;
+        private readonly string streamGenre3;
+        private readonly string streamGenre4;
+        private readonly string streamGenre5;
+        private readonly string streamHomepage;
+        private readonly string streamTitle;
+        private readonly string songTitle;
+        private readonly long streamHits;
+        private readonly int streamStatus;
+        private readonly int backupStatus;
+        private readonly bool streamListed;
+        private readonly string streamPath;
+        private readonly long streamUptime;
+        private readonly int bitRate;
+        private readonly int sampleRate;
+        private readonly string mimeInfo;
+
+        /// <summary>
+        /// Stream ID starting from number one (1)
+        /// </summary>
+        public int StreamId => streamId;
+        /// <summary>
+        /// How many people are listening to the stream at this time?
+        /// </summary>
+        public int CurrentListeners => currentListeners;
+        /// <summary>
+        /// How many listeners did the stream ever get at peak times?
+        /// </summary>
+        public int PeakListeners => peakListeners;
+        /// <summary>
+        /// How many people can listen to the stream?
+        /// </summary>
+        public int MaxListeners => maxListeners;
+        /// <summary>
+        /// How many unique listeners are there?
+        /// </summary>
+        public int UniqueListeners => uniqueListeners;
+        /// <summary>
+        /// Average time on any active listener connections in seconds
+        /// </summary>
+        public int AverageTime => averageTime;
+        /// <summary>
+        /// Average time on any active listener connections in the time span
+        /// </summary>
+        public TimeSpan AverageTimeSpan => TimeSpan.FromSeconds(averageTime);
+        /// <summary>
+        /// The stream genre
+        /// </summary>
+        public string StreamGenre => streamGenre;
+        /// <summary>
+        /// The second stream genre
+        /// </summary>
+        public string StreamGenre2 => streamGenre2;
+        /// <summary>
+        /// The third stream genre
+        /// </summary>
+        public string StreamGenre3 => streamGenre3;
+        /// <summary>
+        /// The fourth stream genre
+        /// </summary>
+        public string StreamGenre4 => streamGenre4;
+        /// <summary>
+        /// The fifth stream genre
+        /// </summary>
+        public string StreamGenre5 => streamGenre5;
+        /// <summary>
+        /// Link to the stream homepage
+        /// </summary>
+        public string StreamHomepage => streamHomepage;
+        /// <summary>
+        /// Stream title
+        /// </summary>
+        public string StreamTitle => streamTitle;
+        /// <summary>
+        /// Song title
+        /// </summary>
+        public string SongTitle => songTitle;
+        /// <summary>
+        /// Stream hits
+        /// </summary>
+        public long StreamHits => streamHits;
+        /// <summary>
+        /// Stream status
+        /// </summary>
+        public int StreamStatus => streamStatus;
+        /// <summary>
+        /// Backup status
+        /// </summary>
+        public int BackupStatus => backupStatus;
+        /// <summary>
+        /// Is the stream listed?
+        /// </summary>
+        public bool StreamListed => streamListed;
+        /// <summary>
+        /// Path to stream
+        /// </summary>
+        public string StreamPath => streamPath;
+        /// <summary>
+        /// Stream uptime in seconds
+        /// </summary>
+        public long StreamUptime => streamUptime;
+        /// <summary>
+        /// Stream uptime in the time span
+        /// </summary>
+        public TimeSpan StreamUptimeSpan => TimeSpan.FromSeconds(StreamUptime);
+        /// <summary>
+        /// Stream bitrate in kbps
+        /// </summary>
+        public int BitRate => bitRate;
+        /// <summary>
+        /// Sampling rate in Hz
+        /// </summary>
+        public int SampleRate => sampleRate;
+        /// <summary>
+        /// MIME info for stream, usually audio/mpeg.
+        /// </summary>
+        public string MimeInfo => mimeInfo;
+
+        /// <summary>
+        /// Makes a new stream information which has all the streams
+        /// </summary>
+        /// <param name="server">Shoutcast server class instance</param>
+        /// <param name="stream">Individual stream token</param>
+        internal StreamInfo(ShoutcastServer server, JToken stream)
+        {
+            try
+            {
+                // Determine version of Shoutcast
+                if (server.ServerVersion == ShoutcastVersion.v1)
+                {
+                    // Shoutcast version v1.x, so use the html fallback token.
+                    string[] response = server.streamHtmlToken.DocumentNode.SelectSingleNode("body").InnerText.Split(',');
+                    currentListeners = Convert.ToInt32(response[0]);
+                    streamStatus = Convert.ToInt32(response[1]);
+                    peakListeners = Convert.ToInt32(response[2]);
+                    maxListeners = Convert.ToInt32(response[3]);
+                    uniqueListeners = Convert.ToInt32(response[4]);
+                    bitRate = Convert.ToInt32(response[5]);
+                    songTitle = response[6];
+                }
+                else
+                {
+                    // Shoutcast version v2.x, so use the JToken.
+                    streamId = (int)stream["id"];
+                    currentListeners = (int)stream["currentlisteners"];
+                    peakListeners = (int)stream["peaklisteners"];
+                    maxListeners = (int)stream["maxlisteners"];
+                    uniqueListeners = (int)stream["uniquelisteners"];
+                    averageTime = (int)stream["averagetime"];
+                    streamGenre = (string)stream["servergenre"];
+                    streamGenre2 = (string)stream["servergenre2"];
+                    streamGenre3 = (string)stream["servergenre3"];
+                    streamGenre4 = (string)stream["servergenre4"];
+                    streamGenre5 = (string)stream["servergenre5"];
+                    streamHomepage = (string)stream["serverurl"];
+                    streamTitle = (string)stream["servertitle"];
+                    songTitle = (string)stream["songtitle"];
+                    streamHits = (int)stream["streamhits"];
+                    streamStatus = (int)stream["streamstatus"];
+                    backupStatus = (int)stream["backupstatus"];
+                    streamListed = (bool)stream["streamlisted"];
+                    streamPath = (string)stream["streampath"];
+                    streamUptime = (int)stream["streamuptime"];
+                    bitRate = (int)stream["bitrate"];
+                    sampleRate = (int)stream["samplerate"];
+                    mimeInfo = (string)stream["content"];
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ShoutcastStreamParseException($"Failed to parse Shoutcast stream ID {streamId}. More information can be found in the inner exception.", ex);
+            }
+        }
+    }
+}
