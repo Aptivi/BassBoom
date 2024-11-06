01369f0180820e5107106a1b53a202d7e34d0c9b
Aptivi <eoflaoevicecity@gmail.com>
2024-11-06 13:35:36 +0300

    bkp - Backported errata fixes
    ---
    
    Taking fixes from BassBoom v0.2.4.2, we've fixed issues related to MPG123
    dependencies.
    
    ---
    
    The following commits or tags are used for this backport:
      - 080533a3d1adbe3c70688ced8564f7cb5dcd770b [v0.2.4.2]
    
    ---
    
    Type: bkp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- BassBoom.Native/MpgNative.cs
+++ BassBoom.Native/MpgNative.cs
@@ -37,6 +37,7 @@ namespace BassBoom.Native
     {
         internal static string mpg123LibPath = GetLibPath("mpg123");
         internal static string out123LibPath = GetLibPath("out123");
+        internal static string pthreadLibPath = GetLibPath("libwinpthread-1");
 
         internal static mpg123_handle* _mpg123Handle;
         internal static out123_handle* _out123Handle;
@@ -122,7 +123,10 @@ namespace BassBoom.Native
             if (architecture == Architecture.X86 || architecture == Architecture.Arm)
                 throw new BasoliaNativeLibraryException("32-bit platforms are no longer supported.");
             libManagerMpg = new LibraryManager(new LibraryFile(mpg123LibPath));
-            libManagerOut = new LibraryManager(new LibraryFile(out123LibPath));
+            libManagerOut = new LibraryManager(
+                architecture == Architecture.X64 && PlatformHelper.IsOnWindows() ?
+                [new LibraryFile(pthreadLibPath), new LibraryFile(out123LibPath)] :
+                [new LibraryFile(out123LibPath)]);
             libManagerMpg.LoadNativeLibrary();
             libManagerOut.LoadNativeLibrary();
 
