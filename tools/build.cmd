ffc3ab9f965f233759d6197f4483713b44cbe915
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-02-25 18:23:36 +0300

    pkg - Download libmpv DLL files for Windows
    ---
    
    We need to download libmpv dll files for Windows, since we can't store big
    files in the repository.
    
    ---
    
    Type: pkg
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/build.cmd
+++ tools/build.cmd
@@ -12,6 +12,16 @@ REM Turn off telemetry and logo
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 set DOTNET_NOLOGO=1
 
+REM Download libmpv for Windows and build
+powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-x86_64-20250225-git-5459b0f.7z -OutFile mpv-dev-x86_64-20250225-git-5459b0f.7z"
+powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-aarch64-20250225-git-5459b0f.7z -OutFile mpv-dev-aarch64-20250225-git-5459b0f.7z"
+"%ProgramFiles%\7-Zip\7z.exe" x mpv-dev-x86_64-20250225-git-5459b0f.7z libmpv-2.dll
+mkdir ..\public\BassBoom.Native\runtimes\win-x64\native\
+mv libmpv-2.dll ..\public\BassBoom.Native\runtimes\win-x64\native\
+"%ProgramFiles%\7-Zip\7z.exe" x mpv-dev-aarch64-20250225-git-5459b0f.7z libmpv-2.dll
+mkdir ..\public\BassBoom.Native\runtimes\win-arm64\native\
+mv libmpv-2.dll ..\public\BassBoom.Native\runtimes\win-arm64\native\
+
 :download
 echo Downloading packages...
 "%ProgramFiles%\dotnet\dotnet.exe" restore "..\BassBoom.sln" -p:Configuration=%releaseconfig% %buildoptions%
