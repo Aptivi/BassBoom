a2eb9d7246b0fa9b51ebaa9e4d671aa120953e49
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-08 15:05:04 +0300

    bkp - Backported build system improvements
    ---
    
    We've backported a series of build system improvements from Aptivestigate
    as it has the most up-to-date files.
    
    ---
    
    The following commits or tags are used for this backport:
      - Aptivi/Aptivestigate@93569e6
      - Aptivi/Aptivestigate@6ffb18b
      - Aptivi/Aptivestigate@a0a12ea
      - Aptivi/Aptivestigate@054d8b1
      - Aptivi/Aptivestigate@bac13b7
    
    ---
    
    Type: bkp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/build.cmd
+++ tools/build.cmd
@@ -12,26 +12,34 @@ REM Turn off telemetry and logo
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 set DOTNET_NOLOGO=1
 
+set ROOTDIR=%~dp0\..
+
 REM Download libmpv for Windows and build
 if not exist %TEMP%\mpv-dev-x86_64-20250225-git-5459b0f.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-x86_64-20250225-git-5459b0f.7z -OutFile %TEMP%\mpv-dev-x86_64-20250225-git-5459b0f.7z"
 if not exist %TEMP%\mpv-dev-aarch64-20250225-git-5459b0f.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-aarch64-20250225-git-5459b0f.7z -OutFile %TEMP%\mpv-dev-aarch64-20250225-git-5459b0f.7z"
+
+pushd %ROOTDIR%\tools\
 "%ProgramFiles%\7-Zip\7z.exe" x %TEMP%\mpv-dev-x86_64-20250225-git-5459b0f.7z libmpv-2.dll
-mkdir ..\public\BassBoom.Native\runtimes\win-x64\native\
-move libmpv-2.dll ..\public\BassBoom.Native\runtimes\win-x64\native\
+popd
+mkdir %ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\
+move %ROOTDIR%\tools\libmpv-2.dll %ROOTDIR%\public\BassBoom.Native\runtimes\win-x64\native\
+
+pushd %ROOTDIR%\tools\
 "%ProgramFiles%\7-Zip\7z.exe" x %TEMP%\mpv-dev-aarch64-20250225-git-5459b0f.7z libmpv-2.dll
-mkdir ..\public\BassBoom.Native\runtimes\win-arm64\native\
-move libmpv-2.dll ..\public\BassBoom.Native\runtimes\win-arm64\native\
+popd
+mkdir %ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\
+move %ROOTDIR%\tools\libmpv-2.dll %ROOTDIR%\public\BassBoom.Native\runtimes\win-arm64\native\
 
 :download
 echo Downloading packages...
-"%ProgramFiles%\dotnet\dotnet.exe" restore "..\BassBoom.sln" -p:Configuration=%releaseconfig% %buildoptions%
+"%ProgramFiles%\dotnet\dotnet.exe" restore "%ROOTDIR%\BassBoom.sln" -p:Configuration=%releaseconfig% %buildoptions%
 if %errorlevel% == 0 goto :build
 echo There was an error trying to download packages (%errorlevel%).
 goto :finished
 
 :build
 echo Building BassBoom...
-"%ProgramFiles%\dotnet\dotnet.exe" build "..\BassBoom.sln" -p:Configuration=%releaseconfig% %buildoptions%
+"%ProgramFiles%\dotnet\dotnet.exe" build "%ROOTDIR%\BassBoom.sln" -p:Configuration=%releaseconfig% %buildoptions%
 if %errorlevel% == 0 goto :success
 echo There was an error trying to build (%errorlevel%).
 goto :finished
--- tools/pack.cmd
+++ tools/pack.cmd
@@ -1,38 +1,23 @@
 @echo off
 
-REM    BassBoom  Copyright (C) 2023  Aptivi
-REM
-REM    This file is part of BassBoom
-REM
-REM    BassBoom is free software: you can redistribute it and/or modify
-REM    it under the terms of the GNU General Public License as published by
-REM    the Free Software Foundation, either version 3 of the License, or
-REM    (at your option) any later version.
-REM
-REM    BassBoom is distributed in the hope that it will be useful,
-REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM    GNU General Public License for more details.
-REM
-REM    You should have received a copy of the GNU General Public License
-REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+set ROOTDIR=%~dp0\..
 
-for /f "tokens=*" %%g in ('findstr "<Version>" ..\Directory.Build.props') do (set MIDVER=%%g)
+for /f "tokens=*" %%g in ('findstr "<Version>" %ROOTDIR%\Directory.Build.props') do (set MIDVER=%%g)
 for /f "tokens=1 delims=<" %%a in ("%MIDVER:~9%") do (set version=%%a)
 set releaseconfig=%1
 if "%releaseconfig%" == "" set releaseconfig=Release
 
 :packbin
 echo Packing binary...
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-cli.zip "..\private\BassBoom.Cli\bin\%releaseconfig%\net8.0\*"
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-cli-48.zip "..\private\BassBoom.Cli\bin\%releaseconfig%\net48\*"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-cli.zip "%ROOTDIR%\private\BassBoom.Cli\bin\%releaseconfig%\net8.0\*"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip %temp%/%version%-cli-48.zip "%ROOTDIR%\private\BassBoom.Cli\bin\%releaseconfig%\net48\*"
 if %errorlevel% == 0 goto :complete
 echo There was an error trying to pack binary (%errorlevel%).
 goto :finished
 
 :complete
-move %temp%\%version%-cli.zip
-move %temp%\%version%-cli-48.zip
+move %temp%\%version%-cli.zip %ROOTDIR%\tools\
+move %temp%\%version%-cli-48.zip %ROOTDIR%\tools\
 
 echo Pack successful.
 :finished
