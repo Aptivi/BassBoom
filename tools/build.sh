30bfaaa5726c02ca5195cb3f4f6d5dd70c4e2017
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-11 20:15:29 +0300

    int - build.sh now downloads WIndows libmpv
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/build.sh
+++ tools/build.sh
@@ -21,10 +21,27 @@ fi
 # Check for dependencies
 dotnetpath=`which dotnet`
 checkerror $? "dotnet is not found"
+sevenzpath=`which 7z`
+checkerror $? "7z is not found"
 
 # Turn off telemetry and logo
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
+
+# Download compiled Windows libmpv libraries
+if [ ! -f /tmp/mpv-dev-x86_64-20250225-git-5459b0f.7z ]; then
+    curl -L --output /tmp/mpv-dev-x86_64-20250225-git-5459b0f.7z https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-x86_64-20250225-git-5459b0f.7z
+fi
+if [ ! -f /tmp/mpv-dev-aarch64-20250225-git-5459b0f.7z ]; then
+    curl -L --output /tmp/mpv-dev-aarch64-20250225-git-5459b0f.7z https://github.com/zhongfly/mpv-winbuild/releases/download/2025-02-25-5459b0f/mpv-dev-aarch64-20250225-git-5459b0f.7z
+fi
+cd $ROOTDIR/tools && "$sevenzpath" x /tmp/mpv-dev-x86_64-20250225-git-5459b0f.7z libmpv-2.dll && cd -
+mkdir -p $ROOTDIR/public/BassBoom.Native/runtimes/win-x64/native/
+mv $ROOTDIR/tools/libmpv-2.dll $ROOTDIR/public/BassBoom.Native/runtimes/win-x64/native/
+
+cd $ROOTDIR/tools && "$sevenzpath" x /tmp/mpv-dev-aarch64-20250225-git-5459b0f.7z libmpv-2.dll && cd -
+mkdir -p $ROOTDIR/public/BassBoom.Native/runtimes/win-arm64/native/
+mv $ROOTDIR/tools/libmpv-2.dll $ROOTDIR/public/BassBoom.Native/runtimes/win-arm64/native/
 
 # Download packages
 echo Downloading packages...
