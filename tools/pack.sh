e920601cca8ebe5ebbfa833bfde6d7267b98ed7f
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-11 19:42:00 +0300

    int - Backported build system improvements
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/pack.sh
+++ tools/pack.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-#   BassBoom  Copyright (C) 2023  Aptivi
-# 
-#   This file is part of BassBoom
-# 
-#   BassBoom is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-# 
-#   BassBoom is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-# 
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Repository root
+ROOTDIR=$( cd -- "$( dirname -- "$0" )/.." &> /dev/null && pwd )
 
 # Convenience functions
 checkerror() {
@@ -26,12 +12,12 @@ checkerror() {
     fi
 }
 
-# This script builds KS and packs the artifacts. Use when you have MSBuild installed.
+# This script builds and packs the artifacts.
 releaseconf=$1
 if [ -z $releaseconf ]; then
 	releaseconf=Release
 fi
-version=$(grep "<Version>" ../Directory.Build.props | cut -d "<" -f 2 | cut -d ">" -f 2)
+version=$(grep "<Version>" $ROOTDIR/Directory.Build.props | cut -d "<" -f 2 | cut -d ">" -f 2)
 checkerror $? "Failed to get version. Check to make sure that the version is specified correctly in D.B.props"
 
 # Check for dependencies
@@ -40,15 +26,15 @@ checkerror $? "zip is not found"
 
 # Pack binary
 echo Packing binary...
-cd "../private/BassBoom.Cli/bin/$releaseconf/net8.0/" && "$zippath" -r /tmp/$version-cli.zip . && cd -
+cd "$ROOTDIR/private/BassBoom.Cli/bin/$releaseconf/net8.0/" && "$zippath" -r /tmp/$version-cli.zip . && cd -
 checkerror $? "Failed to pack"
-cd "../private/BassBoom.Cli/bin/$releaseconf/net48/" && "$zippath" -r /tmp/$version-cli-48.zip . && cd -
+cd "$ROOTDIR/private/BassBoom.Cli/bin/$releaseconf/net48/" && "$zippath" -r /tmp/$version-cli-48.zip . && cd -
 checkerror $? "Failed to pack"
 
 # Inform success
-mv /tmp/$version-cli.zip .
+mv /tmp/$version-cli.zip $ROOTDIR/tools/
 checkerror $? "Failed to move archive from temporary folder"
-mv /tmp/$version-cli-48.zip .
+mv /tmp/$version-cli-48.zip $ROOTDIR/tools/
 checkerror $? "Failed to move archive from temporary folder"
 echo Build and pack successful.
 exit 0
