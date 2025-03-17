7ca7e76d94d26b500a8f5f2ffe96e59e224e1ffe
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-17 14:40:08 +0300

    imp - Improved player appearance
    ---
    
    We've improved the player appearance by renewing the look of elements.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- private/BassBoom.Cli/CliBase/Player.cs
+++ private/BassBoom.Cli/CliBase/Player.cs
@@ -112,7 +112,7 @@ namespace BassBoom.Cli.CliBase
 
                 // Get the positions and the amount of songs per page
                 int startPos = 4;
-                int endPos = ConsoleWrapper.WindowHeight - 5;
+                int endPos = ConsoleWrapper.WindowHeight - 3;
                 int songsPerPage = endPos - startPos;
 
                 // Get the position
@@ -138,19 +138,18 @@ namespace BassBoom.Cli.CliBase
                     InteriorHeight = songsPerPage,
                     FrameColor = disco,
                     TitleColor = disco,
-                    BackgroundColor = disco,
                 };
                 var durationBar = new SimpleProgress((int)(100 * (position / (double)Common.CurrentCachedInfo.Duration)), 100)
                 {
-                    LeftMargin = 1,
-                    RightMargin = 1,
+                    LeftMargin = 2,
+                    RightMargin = 2,
                     ShowPercentage = false,
                     ProgressForegroundColor = TransformationTools.GetDarkBackground(disco),
                     ProgressActiveForegroundColor = disco,
                 };
                 buffer.Append(
                     listBoxFrame.Render() +
-                    ContainerTools.RenderRenderable(listBoxFrame, new(2, ConsoleWrapper.WindowHeight - 5))
+                    ContainerTools.RenderRenderable(durationBar, new(2, ConsoleWrapper.WindowHeight - 3))
                 );
 
                 // Render the indicator
@@ -471,7 +470,7 @@ namespace BassBoom.Cli.CliBase
             // Now, populate the input choice information instances that represent songs
             var choices = new List<InputChoiceInfo>();
             int startPos = 4;
-            int endPos = ConsoleWrapper.WindowHeight - 5;
+            int endPos = ConsoleWrapper.WindowHeight - 3;
             int songsPerPage = endPos - startPos;
             int max = Common.cachedInfos.Select((_, idx) => idx).Max((idx) => $"  {idx + 1}) ".Length);
             for (int i = 0; i < Common.cachedInfos.Count; i++)
