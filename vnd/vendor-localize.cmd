21b835c4549b2df3dea6dbdb2e9c088929855d5a
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-04-02 21:26:54 +0300

    bkp - Backported localize scripts
    ---
    
    The localize vendor scripts have been backported from Nitrocid and
    adjusted to work with BassBoom.
    
    ---
    
    Type: bkp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ vnd/vendor-localize.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+set ROOTDIR=%~dp0\..
+set ROOTDIRFIND=%~dp0..
+
+echo Restoring NuGet packages...
+"%ProgramFiles%\dotnet\dotnet.exe" restore "%ROOTDIR%\BassBoom.sln" --packages "%ROOTDIR%\nuget"
+if %errorlevel% == 0 goto :copy
+echo There was an error trying to restore packages (%errorlevel%).
+goto :finished
+
+:copy
+echo Copying dependencies...
+md "%ROOTDIR%\deps"
+forfiles /s /m *.nupkg /p "%ROOTDIRFIND%\nuget\" /C "cmd /c copy @path ""%ROOTDIR%\deps"""
+forfiles /s /m *.nupkg /p "%USERPROFILE%\.nuget\packages\" /C "cmd /c copy @path ""%ROOTDIR%\deps"""
+if %errorlevel% == 0 goto :initconfig
+echo There was an error trying to copy dependencies (%errorlevel%).
+goto :finished
+
+:initconfig
+echo "<?xml version=""1.0"" encoding=""utf-8""?>" > %ROOTDIR%\NuGet.config
+echo "<configuration>" >> %ROOTDIR%\NuGet.config
+echo "  <packageSources>" >> %ROOTDIR%\NuGet.config
+echo "    <clear />" >> %ROOTDIR%\NuGet.config
+echo "    <add key=""nuget.org"" value=""%ROOTDIR%/deps"" />" >> %ROOTDIR%\NuGet.config
+echo "  </packageSources>" >> %ROOTDIR%\NuGet.config
+echo "</configuration>" >> %ROOTDIR%\NuGet.config
+rd /s /q "%ROOTDIR%\nuget"
+if %errorlevel% == 0 goto :success
+echo There was an error trying to delete cache (%errorlevel%).
+goto :finished
+
+:success
+echo Localization successful.
+
+:finished
