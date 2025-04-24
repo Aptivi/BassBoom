b03cfab88d68e68e4c4430508ad8ec6597153485
AptiviCEO <AptiviCEO@users.noreply.github.com>
2025-04-24 12:54:34 +0000

    int - Updated libmpv links

--- vnd/vendor-prelocalize.cmd
+++ vnd/vendor-prelocalize.cmd
@@ -3,5 +3,5 @@
 set ROOTDIR=%~dp0\..
 
 REM Download libmpv for Windows and build
-if not exist %TEMP%\mpv-dev-x86_64-20250423-git-f147df1.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-04-23-f147df1/mpv-dev-x86_64-20250423-git-f147df1.7z -OutFile %TEMP%\mpv-dev-x86_64-20250423-git-f147df1.7z"
-if not exist %TEMP%\mpv-dev-aarch64-20250423-git-f147df1.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-04-23-f147df1/mpv-dev-aarch64-20250423-git-f147df1.7z -OutFile %TEMP%\mpv-dev-aarch64-20250423-git-f147df1.7z"
+if not exist %TEMP%\mpv-dev-x86_64-20250424-git-f147df1.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-04-24-f147df1/mpv-dev-x86_64-20250424-git-f147df1.7z -OutFile %TEMP%\mpv-dev-x86_64-20250424-git-f147df1.7z"
+if not exist %TEMP%\mpv-dev-aarch64-20250424-git-f147df1.7z powershell -Command "Invoke-WebRequest https://github.com/zhongfly/mpv-winbuild/releases/download/2025-04-24-f147df1/mpv-dev-aarch64-20250424-git-f147df1.7z -OutFile %TEMP%\mpv-dev-aarch64-20250424-git-f147df1.7z"
