a703a86b2a33f6fc28efff39974bd89ab103db64
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-06-27 07:36:14 +0300

    pkg - Handle paths with spaces in the incrementor
    ---
    
    Type: pkg
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- vnd/vendor-increment.cmd
+++ vnd/vendor-increment.cmd
@@ -27,15 +27,15 @@ REM This script replaces old version with new version.
 echo Replacing...
 
 REM Change the below files
-set "releaseFiles=%ROOTDIR%\PKGBUILD-REL %ROOTDIR%\.github\workflows\build-ppa-package-with-lintian.yml %ROOTDIR%\.github\workflows\build-ppa-package.yml %ROOTDIR%\.github\workflows\pushamend.yml %ROOTDIR%\.github\workflows\pushppa.yml %ROOTDIR%\public\BassBoom.Installers\BassBoom.Installer\Package.wxs %ROOTDIR%\public\BassBoom.Installers\BassBoom.InstallerBundle\Bundle.wxs %ROOTDIR%\Directory.Build.props %ROOTDIR%\CHANGES.TITLE"
+set "releaseFiles="%ROOTDIR%\PKGBUILD-REL" "%ROOTDIR%\.github\workflows\build-ppa-package-with-lintian.yml" "%ROOTDIR%\.github\workflows\build-ppa-package.yml" "%ROOTDIR%\.github\workflows\pushamend.yml" "%ROOTDIR%\.github\workflows\pushppa.yml" "%ROOTDIR%\public\BassBoom.Installers\BassBoom.Installer\Package.wxs" "%ROOTDIR%\public\BassBoom.Installers\BassBoom.InstallerBundle\Bundle.wxs" "%ROOTDIR%\Directory.Build.props" "%ROOTDIR%\CHANGES.TITLE""
 for %%f in (%releaseFiles%) do (
     echo Processing %%f...
-    powershell %ROOTDIR%\vnd\eng\incrementor.ps1 "%%f" "%oldVer%" "%newVer%" "%oldApiVer%" "%newApiVer%"
+    powershell -Command "& '%ROOTDIR%\vnd\eng\incrementor.ps1' '%%f' '%oldVer%' '%newVer%' '%oldApiVer%' '%newApiVer%'"
 )
 
 REM Run the version changer
-powershell %ROOTDIR%\vnd\eng\bb-version-change.ps1 "%ROOTDIR%" "%oldApiVer%" "%newApiVer%"
+powershell -Command "& '%ROOTDIR%\vnd\eng\bb-version-change.ps1' '%ROOTDIR%' '%oldApiVer%' '%newApiVer%'"
 
 REM Add Debian changelogs info
 echo Changing Debian changelogs info
-powershell %ROOTDIR%\vnd\eng\debian-changes.ps1 "%ROOTDIR%\debian\changelog" "%newVer%" "%newApiVer%"
+powershell -Command "& '%ROOTDIR%\vnd\eng\debian-changes.ps1' '%ROOTDIR%\debian\changelog' '%newVer%' '%newApiVer%'"
